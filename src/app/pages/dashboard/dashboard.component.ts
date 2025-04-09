b04e1951ab47aa2c3a1c26c671f7f2cd03f55f00
DESKTOP-5SKO262\Vimal Nirmal <vimalrock82@gmail.com>
2025-04-09 12:39:57 +0530

    Added condition to get cart on dashboard

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -5,102 +5,104 @@ import { AppService } from '../../app.service';
 import { ToastrService } from 'ngx-toastr';
 
 @Component({
-    selector: 'app-dashboard',
-    templateUrl: './dashboard.component.html',
-    styleUrl: './dashboard.component.scss'
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent implements OnInit {
 
-    user: User = {};
-    productsList: Product[] = [];
-    cart: Cart = {};
-    isLoggedIn: boolean = false;
-    loading: boolean = true;
+  user: User = {};
+  productsList: Product[] = [];
+  cart: Cart = {};
+  isLoggedIn: boolean = false;
+  loading: boolean = true;
 
-    constructor(
-        private appService: AppService,
-        private loaderService: NgxUiLoaderService,
-        private toastrService: ToastrService
-    ) {
+  constructor(
+    private appService: AppService,
+    private loaderService: NgxUiLoaderService,
+    private toastrService: ToastrService
+  ) {
 
+  }
+
+  ngOnInit(): void {
+    this.verifyUserLogin();
+  }
+
+  verifyUserLogin() {
+    this.user = this.appService.getUserData('user') || {};
+    if (this.user && this.user.id) {
+      this.isLoggedIn = true;
     }
-
-    ngOnInit(): void {
-        this.verifyUserLogin();
+    this.getProducts();
+    if (this.isLoggedIn) {
+      this.getCart();
     }
+  }
 
-    verifyUserLogin() {
-        this.user = this.appService.getUserData('user') || {};
-        if (this.user && this.user.id) {
-            this.isLoggedIn = true;
+  getProducts() {
+    this.loading = true;
+    this.loaderService.start();
+    this.appService.getProducts().subscribe({
+      next: (res: any) => {
+        if (res) {
+          this.productsList = this.appService.parseProductsList(res.data);
         }
-        this.getProducts();
-        this.getCart();
-    }
+      },
+      error: (error) => {
+        this.loading = false;
+        this.loaderService.stop();
+      },
+      complete: () => {
+        this.loading = false;
+        this.loaderService.stop();
+      }
+    });
+  }
 
-    getProducts() {
-        this.loading = true;
-        this.loaderService.start();
-        this.appService.getProducts().subscribe({
-            next: (res: any) => {
-                if (res) {
-                    this.productsList = this.appService.parseProductsList(res.data);
-                }
-            },
-            error: (error) => {
-                this.loading = false;
-                this.loaderService.stop();
-            },
-            complete: () => {
-                this.loading = false;
-                this.loaderService.stop();
-            }
-        });
-    }
+  getCart() {
+    this.loaderService.start();
+    const payload = {
+      account_id: this.user.id
+    };
+    this.appService.getCart(payload).subscribe({
+      next: (res: any) => {
+        if (res && res.success) {
+          this.cart = this.appService.parseCart(res.data);
+        }
+      },
+      error: (error) => {
+        this.loaderService.stop();
+      },
+      complete: () => {
+        this.loaderService.stop();
+      }
+    });
+  }
 
-    getCart() {
-        this.loaderService.start();
-        const payload = {
-            account_id: this.user.id
-        };
-        this.appService.getCart(payload).subscribe({
-            next: (res: any) => {
-                if (res && res.success) {
-                    this.cart = this.appService.parseCart(res.data);
-                }
-            },
-            error: (error) => {
-                this.loaderService.stop();
-            },
-            complete: () => {
-                this.loaderService.stop();
-            }
-        });
-    }
-
-    addToCart(product: Product, event: Event): void {
-        event.stopPropagation();
-        this.loaderService.start();
-        const payload = {
-            account_id: this.user.id,
-            cart_item: {
-                product: product,
-                qty: 1
-            }
-        };
-        this.appService.addToCart(payload).subscribe({
-            next: (res: any) => {
-                if (res && res.success) {
-                    this.toastrService.success("Added to the cart successfully!");
-                    this.getCart();
-                }
-            },
-            error: (error) => {
-                this.loaderService.stop();
-            },
-            complete: () => {
-                this.loaderService.stop();
-            }
-        });
-    }
+  addToCart(product: Product, event: Event): void {
+    event.stopPropagation();
+    this.loaderService.start();
+    const payload = {
+      account_id: this.user.id,
+      cart_item: {
+        product: product,
+        qty: 1
+      }
+    };
+    this.appService.addToCart(payload).subscribe({
+      next: (res: any) => {
+        if (res && res.success) {
+          this.toastrService.success("Added to the cart successfully!");
+          this.getCart();
+        }
+      },
+      error: (error) => {
+        this.loaderService.stop();
+      },
+      complete: () => {
+        this.loaderService.stop();
+      }
+    });
+  }
 }
